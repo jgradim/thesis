a0221a555909337304900d0cb0d913bc02e45dd4
João Gradim <joao.gradim@gmail.com>
2010-11-18 16:37:26 +0000

    Updated gitignore

--- prototypes/documents/app/models/version.rb
+++ prototypes/documents/app/models/version.rb
@@ -1,17 +1,26 @@
 class Version < ActiveRecord::Base
 
+  def object
+    HashObject.new(raw_object)
+  end
+  
+  def raw_object
+    JSON.load(content)
+  end
+  
+  #
   class HashObject
-    def initialize(hash)
-      @hash = hash
+    def initialize(h)
+      @h = h
+      @h.each do |k,v|
+        #puts k,v
+        @h[k] = HashObject.new(v) if v.is_a?(Hash)
+      end
     end
     
     def method_missing(meth, *args, &block)
-      @hash[meth]
+      return @h[meth.to_s]
     end
-  end
-
-  def object
-    JSON.load(self.content)
   end
 
 end
