a7e4915edaac7ce55babae5ca2d1858af43e5f2b
João Gradim <joao.gradim@gmail.com>
2010-11-18 16:51:43 +0000

    Added Version::CallableHash class in order to easily navigate version objects

--- prototypes/documents/app/models/version.rb
+++ prototypes/documents/app/models/version.rb
@@ -1,7 +1,7 @@
 class Version < ActiveRecord::Base
 
   def object
-    HashObject.new(raw_object)
+    CallableHash.new(raw_object)
   end
   
   def raw_object
@@ -9,18 +9,22 @@ class Version < ActiveRecord::Base
   end
   
   #
-  class HashObject
+  class CallableHash
+  
     def initialize(h)
       @h = h
       @h.each do |k,v|
-        #puts k,v
-        @h[k] = HashObject.new(v) if v.is_a?(Hash)
+        @h[k] = CallableHash.new(v) if v.is_a?(Hash)
+        @h[k] = v.map { |o|
+          o.is_a?(Hash) ? CallableHash.new(o) : o
+        } if v.is_a?(Array)
       end
     end
     
     def method_missing(meth, *args, &block)
       return @h[meth.to_s]
     end
+    
   end
 
 end
