0efa174746a9ae686ace3180780cea68bdaf523d
João Gradim <joao.gradim@gmail.com>
2010-11-30 16:59:12 +0000

    Commented use of yajl-ruby to encode/decode JSON

--- prototypes/documents/config/environment.rb
+++ prototypes/documents/config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   #config.gem 'sqlite3-ruby', :lib => 'sqlite3'
   config.gem 'mysql2'
   config.gem 'acts_as_list'
+  #config.gem 'yajl-ruby', :lib => 'yajl'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
