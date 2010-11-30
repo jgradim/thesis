aa3c0bafc1e374abae0787fa2455856042ea8cba
João Gradim <joao.gradim@gmail.com>
2010-11-30 16:58:44 +0000

    Moved CallableHash to its own class

--- prototypes/documents/app/models/version.rb
+++ prototypes/documents/app/models/version.rb
@@ -5,26 +5,7 @@ class Version < ActiveRecord::Base
   end
   
   def raw_object
-    JSON.load(content)
-  end
-  
-  #
-  class CallableHash
-  
-    def initialize(h)
-      @h = h
-      @h.each do |k,v|
-        @h[k] = CallableHash.new(v) if v.is_a?(Hash)
-        @h[k] = v.map { |o|
-          o.is_a?(Hash) ? CallableHash.new(o) : o
-        } if v.is_a?(Array)
-      end
-    end
-    
-    def method_missing(meth, *args, &block)
-      return @h[meth.to_s]
-    end
-    
+    JSON.parse(content)
   end
 
 end
---
+++ prototypes/documents/lib/callable_hash.rb
@@ -0,0 +1,18 @@
+#
+class CallableHash
+
+  def initialize(h)
+    @h = h
+    @h.each do |k,v|
+      @h[k] = CallableHash.new(v) if v.is_a?(Hash)
+      @h[k] = v.map { |o|
+        o.is_a?(Hash) ? CallableHash.new(o) : o
+      } if v.is_a?(Array)
+    end
+  end
+  
+  def method_missing(meth, *args, &block)
+    return @h[meth.to_s]
+  end
+  
+end
