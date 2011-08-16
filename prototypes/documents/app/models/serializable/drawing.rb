f68822ebfab2655617eccad8a39e938ec027b35b
João Gradim <joao.gradim@gmail.com>
2011-08-16 19:31:19 -0400

    cleanup

--- prototypes/documents/app/models/serializable/drawing.rb
+++ prototypes/documents/app/models/serializable/drawing.rb
@@ -1,10 +1,10 @@
 class Serializable::Drawing < Serializable
 
   attr_accessor :content
-  
-  # attachments
+
+  # drawing png
   extend CarrierWave::Mount
   mount_uploader :attachment, DocumentUploader
   attr_accessor :static_attachment_url
-  
+
 end
--- prototypes/documents/app/models/serializable/paragraph.rb
+++ prototypes/documents/app/models/serializable/paragraph.rb
@@ -1,8 +1,8 @@
 class Serializable::Paragraph < Serializable
-  
+
   attr_accessor :content, :title
-  
-  defaults :title,   Proc.new{ I18n.t('globals.untitled') } #"Sem título"
+
+  defaults :title,   Proc.new{ I18n.t('globals.untitled') } # "Sem título"
   defaults :content, ""
-  
+
 end
