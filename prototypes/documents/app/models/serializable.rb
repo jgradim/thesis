21196e837d856e0976530afce675527801de348b
João Gradim <joao.gradim@gmail.com>
2011-08-23 16:54:33 -0300

    convenience method to store attachments in Serializable:: objects

--- prototypes/documents/app/models/serializable.rb
+++ prototypes/documents/app/models/serializable.rb
@@ -3,8 +3,12 @@ class Serializable
   attr_accessor :block_id # used for file attachments
 
   include InheritableTraits
-  traits :default_values, :validations, :allowed_extensions
+  traits :default_values, :validations, :allowed_extensions, :attrs_with_storage
 
+  #
+  # BUILDER METHODS
+  #
+  ############################################################################
   def initialize(params = {})
 
     self.class.default_values.merge(params).each do |k,v|
@@ -32,11 +36,16 @@ class Serializable
 
   end
 
-  # builder method
+  # reads a JSON-serialized object and tries to create a Serializable:: object from that
   def self.from_json(j)
     j = JSON.parse(j) if j.is_a?(String)
     j.first.first.constantize.new(j.first.last)
   end
+
+  #
+  # CONFIGURATION CLASS METHODS
+  #
+  ############################################################################
 
   # reference to AR models
   # automatically creates attr_accessor and methods for retrieval
@@ -54,7 +63,10 @@ class Serializable
 
   end
 
+  # sets up attributes for reding and setting attachments with CarrierWave
   def self.attaches(attachment_name, opts = {})
+
+    @attrs_with_storage << attachment_name
 
     extend CarrierWave::Mount
     attr_accessor "static_#{attachment_name.to_s}_url"
@@ -73,6 +85,11 @@ class Serializable
     self.validations.merge!({attribute => rule})
   end
 
+  #
+  # INSTANCE MANIPULATION METHODS
+  #
+  ############################################################################
+
   # serialize with class name so object can be rebuilt
   def to_json(*args, &block)
     # Remove the @_mounters instance var added by CarrierWave (0.4.1 atm),
@@ -80,11 +97,32 @@ class Serializable
     "{\"#{self.class}\": #{instance_values.except('_mounters').to_json}}"
   end
 
+  def update_attributes(params = {})
+    params.each do |a, v|
+      instance_variable_set("@#{a}", v)
+    end
+    self.store_attachments(params)
+  end
+
+  def store_attachments(params = {})
+    self.class.attrs_with_storage.each do |att|
+      puts "self.#{att}: #{self.send(att).inspect}"
+      puts "self.#{att}: #{self.send(att)}"
+      self.send(att).store!
+    end
+  end
+
+  #
+  # HELPER METHODS
+  #
+  ############################################################################
+
   # initialize inheritable traits
   def self.inherited(subclass)
     subclass.default_values ||= {}
     subclass.validations ||= {}
     subclass.allowed_extensions ||= []
+    subclass.attrs_with_storage ||= []
   end
 
   # exception classes
